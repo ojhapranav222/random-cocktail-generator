da1bf809adeeb45e7751c1cd317a74a31ec45583
Pranav Ojha <ojhapranav222@gmail.com>
2024-09-24 22:18:11 +0530

    some bug fixes

--- index.js
+++ index.js
@@ -1,50 +1,60 @@
+// index.js
 import express from "express";
 import axios from "axios";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = 3000;
 
-app.use(express.static("public"))
+// Static files
+app.use(express.static(path.join("public")));
 
-app.get("/", async(req, res) => {
-    try{
-        const result = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
-        const id = result.data.drinks[0].idDrink;
-        const title = result.data.drinks[0].strDrink;
-        const imageURL = result.data.drinks[0].strDrinkThumb;
-        const drinkQuantity = [];
-        const drinkIngredients = [];
+// Views
+app.set("view engine", "ejs");
+app.set("views", path.join("views"));
 
-        for (let i = 1; i<=15; i++){
-            const ingredientKey = `strIngredient${i}`;
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+    const id = result.data.drinks[0].idDrink;
+    const title = result.data.drinks[0].strDrink;
+    const imageURL = result.data.drinks[0].strDrinkThumb;
+    const drinkQuantity = [];
+    const drinkIngredients = [];
 
-            if (result.data.drinks[0][ingredientKey]){
-                drinkIngredients.push(result.data.drinks[0][ingredientKey])
-            }
-        }
-        for (let i = 1; i<=15; i++){
-            const measureKey = `strMeasure${i}`;
+    for (let i = 1; i <= 15; i++) {
+      const ingredientKey = `strIngredient${i}`;
+      if (result.data.drinks[0][ingredientKey]) {
+        drinkIngredients.push(result.data.drinks[0][ingredientKey]);
+      }
+    }
+    for (let i = 1; i <= 15; i++) {
+      const measureKey = `strMeasure${i}`;
+      if (result.data.drinks[0][measureKey]) {
+        drinkQuantity.push(result.data.drinks[0][measureKey]);
+      }
+    }
 
-            if (result.data.drinks[0][measureKey]){
-                drinkQuantity.push(result.data.drinks[0][measureKey])
-            }
-        }
+    const data = {
+      id: id,
+      image: imageURL,
+      title: title,
+      ingredients: drinkIngredients,
+      quantity: drinkQuantity,
+    };
 
-        const data = {id: id,
-                    image: imageURL,
-                    title: title,
-                    ingredients: drinkIngredients,
-                    quantity: drinkQuantity,
-                    }
-
-        res.render("index.ejs", data)
-    } catch(error){
-        console.error("cannot fetch data");
-    } 
-})
+    res.render("index", data);
+  } catch (error) {
+    res.status(500).send("Error fetching data");
+  }
+});
 
 app.post("/random", async (req, res) => {
-    try{
+    try {
         const result = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
         const id = result.data.drinks[0].idDrink;
         const title = result.data.drinks[0].strDrink;
@@ -52,34 +62,36 @@ app.post("/random", async (req, res) => {
         const drinkQuantity = [];
         const drinkIngredients = [];
 
-        for (let i = 1; i<=15; i++){
+        for (let i = 1; i <= 15; i++) {
             const ingredientKey = `strIngredient${i}`;
 
-            if (result.data.drinks[0][ingredientKey]){
-                drinkIngredients.push(result.data.drinks[0][ingredientKey])
+            if (result.data.drinks[0][ingredientKey]) {
+                drinkIngredients.push(result.data.drinks[0][ingredientKey]);
             }
         }
-        for (let i = 1; i<=15; i++){
+        for (let i = 1; i <= 15; i++) {
             const measureKey = `strMeasure${i}`;
 
-            if (result.data.drinks[0][measureKey]){
-                drinkQuantity.push(result.data.drinks[0][measureKey])
+            if (result.data.drinks[0][measureKey]) {
+                drinkQuantity.push(result.data.drinks[0][measureKey]);
             }
         }
 
-        const data = {id: id,
-                    image: imageURL,
-                    title: title,
-                    ingredients: drinkIngredients,
-                    quantity: drinkQuantity,
-                    }
+        const data = {
+            id: id,
+            image: imageURL,
+            title: title,
+            ingredients: drinkIngredients,
+            quantity: drinkQuantity,
+        };
 
-        res.render("index.ejs", data)
-    } catch(error){
-        console.error("cannot fetch data");
+        res.render("index", data);
+    } catch (error) {
+        console.error("Cannot fetch data");
+        res.status(500).send("Server Error");
     }
-})
+});
 
 app.listen(port, () => {
-    console.log(`Your server is running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
